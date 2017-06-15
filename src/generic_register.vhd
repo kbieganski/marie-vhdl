34c7758d786a75011ed1c0a40a8b3cf4d76bd62c
Krzysztof Biegański <kbieganski95@gmail.com>
2017-06-15 16:49:12 +0200

    Fixed sending signed integers of lower width than the bus width

--- src/generic_register.vhd
+++ src/generic_register.vhd
@@ -66,7 +66,7 @@ begin
 	end process;
 
 	input <= system_bus when sending = '0' else (others => 'Z');
-	system_bus(bus_width - 1 downto register_width) <= (others => '0') when sending = '1' else (others => 'Z');
+	system_bus(bus_width - 1 downto register_width) <= (others => value(register_width - 1)) when sending = '1' else (others => 'Z');
 	system_bus(register_width - 1 downto 0) <= value when sending = '1' else (others => 'Z');
 	aux_read <= value;
 
